6c5454207b65ab3e76d0a0798c89d77d3ff25b93
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 18:40:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil 
   name_hash.each do |k, v|
-    if name_hash[k][v] < smallest_value_of_set
+    if smallest_value == nil || if 
   end
   end
   smallest_key
